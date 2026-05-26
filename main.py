ee4057bf48a95d972f9b283e170a02be04a97d6c
edroptyul1-svg <edroptyul1@gmail.com>
2026-05-26 13:03:24 +0700

    add main application

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Webhook monitoring tool with alerting and payload logging"""
+
+class App:
+    def __init__(self):
+        self.config = {}
+    
+    def run(self):
+        print("Running webhook-monitor...")
+        return True
+
+if __name__ == "__main__":
+    app = App()
+    app.run()
